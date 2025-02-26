bc64355e2adc864117477258cae1f3cf29a1b671
Carlo Elvisi <carloelvisi01@outlook.it>
2025-02-26 12:37:22 +0100

    update snack 6

--- main.js
+++ main.js
@@ -84,6 +84,8 @@ function creaContatoreAutomatico(intervallo) {
       console.log("Contatore fermato!");
     };
   }
+  const contaOgniSecondo = creaContatoreAutomatico(1000);
+  contaOgniSecondo();
   /* Crea una funzione che ferma un timer dopo un certo tempo
 
     Scrivi una funzione eseguiEferma che accetta un messaggio, un tempo di avvio e un tempo di stop. Il messaggio deve essere stampato a intervalli regolari, ma si deve fermare dopo il tempo di stop.
